ded67bcbe444fccfab96819db9155e593c2afa3c
Acquilla Allen <1stladyofkrypton@gmail.com>
2016-11-27 22:43:02 -0500

    Getting Started

---
+++ FPBeta/FPBeta/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FPBeta.Startup))]
+namespace FPBeta
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
